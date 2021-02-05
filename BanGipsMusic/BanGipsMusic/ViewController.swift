ba1e5bcc879796ef09a34c4f15ff3201e48a8fe6
BanGips <bangips@MacBook-Pro-BanGips.local>
2021-02-05 19:35:57 +0300

    Add Project

---
+++ BanGipsMusic/BanGipsMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BanGipsMusic
+//
+//  Created by BanGips on 5.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
