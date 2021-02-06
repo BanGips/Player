6adf38bfe19bdd98d61093d7eb01d108a234b335
BanGips <bangips@MacBook-Pro-BanGips.local>
2021-02-06 19:42:05 +0300

    MainTabBarController

---
+++ BanGipsMusic/BanGipsMusic/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  BanGipsMusic
+//
+//  Created by BanGips on 5.02.21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let searchVC = SearchViewController()
+        let libraryVC = ViewController()
+        tabBar.tintColor = .magenta
+        
+        viewControllers = [
+            generateNavigationController(rootVC: searchVC, title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(rootVC: libraryVC, title: "Library", image: #imageLiteral(resourceName: "library"))
+        ]
+    }
+    
+    private func generateNavigationController(rootVC: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootVC)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        rootVC.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+}
